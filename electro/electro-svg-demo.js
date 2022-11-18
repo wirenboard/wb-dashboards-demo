d9466cf75885c03c4b3411c9dcfa45523cc28fcd
wb-adegtyarev <a.degtyarev@wirenboard.com>
2022-11-18 13:28:58 +0400

    update

--- electro/electro-svg-demo.js
+++ electro/electro-svg-demo.js
@@ -1,20 +1,20 @@
-var emulator = require("device-emulator"); // подключаем модуль  
+var emulator = require("device-emulator"); // подключаем модуль
 var devices = [ // описываем устройства
     {
         "type": "WB-MAP3E",
         "address": 1,
         "emulators": [
-            { "topicName": "Irms L1", "type": "sensor2Dec", "name": "Irms L1", "lastValue": 39, "min": 38, "max": 42, "interval": 3000 }, // interval (ms) >=1000 ms
+            { "topicName": "Irms L1", "type": "sensor2Dec", "name": "Irms L1", "lastValue": 390, "min": 380, "max": 420, "interval": 3000 }, // interval (ms) >=1000 ms
             { "topicName": "Urms L1", "type": "sensor2Dec", "name": "Urms L1", "lastValue": 220, "min": 210, "max": 225, "interval": 3000 },
             { "topicName": "Phase angle L1", "type": "sensor2Dec", "name": "Phase angle L1", "lastValue": 5.05, "min": 4.80, "max": 5.10, "interval": 3000 },
             { "topicName": "Voltage angle L1", "type": "sensor2Dec", "name": "Phase angle L1", "lastValue": 0, "min": 0, "max": 0, "interval": 3000 },
 
-            { "topicName": "Irms L2", "type": "sensor2Dec", "name": "Irms L2", "lastValue": 12, "min": 10, "max": 15, "interval": 3000 },
+            { "topicName": "Irms L2", "type": "sensor2Dec", "name": "Irms L2", "lastValue": 370, "min": 350, "max": 390, "interval": 3000 },
             { "topicName": "Urms L2", "type": "sensor2Dec", "name": "Urms L2", "lastValue": 222, "min": 210, "max": 225, "interval": 3000 },
             { "topicName": "Phase angle L2", "type": "sensor2Dec", "name": "Phase angle L2", "lastValue": 5.07, "min": 4.80, "max": 5.10, "interval": 3000 },
             { "topicName": "Voltage angle L2", "type": "sensor2Dec", "name": "Phase angle L2", "lastValue": 120, "min": 118, "max": 122, "interval": 3000 },
 
-            { "topicName": "Irms L3", "type": "sensor2Dec", "name": "Irms L3", "lastValue": 16, "min": 14, "max": 20, "interval": 3000 },
+            { "topicName": "Irms L3", "type": "sensor2Dec", "name": "Irms L3", "lastValue": 420, "min": 400, "max": 440, "interval": 3000 },
             { "topicName": "Urms L3", "type": "sensor2Dec", "name": "Urms L3", "lastValue": 219, "min": 210, "max": 225, "interval": 3000 },
             { "topicName": "Phase angle L3", "type": "sensor2Dec", "name": "Phase angle L3", "lastValue": 5.04, "min": 4.80, "max": 5.10, "interval": 3000 },
 
@@ -25,17 +25,17 @@ var devices = [ // описываем устройства
         "type": "WB-MAP3E",
         "address": 2,
         "emulators": [
-            { "topicName": "Irms L1", "type": "sensor2Dec", "name": "Irms L1", "lastValue": 39, "min": 38, "max": 42, "interval": 3000 }, // interval (ms) >=1000 ms
+            { "topicName": "Irms L1", "type": "sensor2Dec", "name": "Irms L1", "lastValue": 390, "min": 380, "max": 420, "interval": 3000 }, // interval (ms) >=1000 ms
             { "topicName": "Urms L1", "type": "sensor2Dec", "name": "Urms L1", "lastValue": 220, "min": 210, "max": 225, "interval": 3000 },
             { "topicName": "Phase angle L1", "type": "sensor2Dec", "name": "Phase angle L1", "lastValue": 5.05, "min": 4.80, "max": 5.10, "interval": 3000 },
             { "topicName": "Voltage angle L1", "type": "sensor2Dec", "name": "Phase angle L1", "lastValue": 0, "min": 0, "max": 0, "interval": 3000 },
 
-            { "topicName": "Irms L2", "type": "sensor2Dec", "name": "Irms L2", "lastValue": 12, "min": 10, "max": 15, "interval": 3000 },
+            { "topicName": "Irms L2", "type": "sensor2Dec", "name": "Irms L2", "lastValue": 370, "min": 350, "max": 390, "interval": 3000 },
             { "topicName": "Urms L2", "type": "sensor2Dec", "name": "Urms L2", "lastValue": 222, "min": 210, "max": 225, "interval": 3000 },
             { "topicName": "Phase angle L2", "type": "sensor2Dec", "name": "Phase angle L2", "lastValue": 5.07, "min": 4.80, "max": 5.10, "interval": 3000 },
             { "topicName": "Voltage angle L2", "type": "sensor2Dec", "name": "Phase angle L2", "lastValue": 120, "min": 118, "max": 122, "interval": 3000 },
 
-            { "topicName": "Irms L3", "type": "sensor2Dec", "name": "Irms L3", "lastValue": 16, "min": 14, "max": 20, "interval": 3000 },
+            { "topicName": "Irms L3", "type": "sensor2Dec", "name": "Irms L3", "lastValue": 420, "min": 400, "max": 440, "interval": 3000 },
             { "topicName": "Urms L3", "type": "sensor2Dec", "name": "Urms L3", "lastValue": 219, "min": 210, "max": 225, "interval": 3000 },
             { "topicName": "Phase angle L3", "type": "sensor2Dec", "name": "Phase angle L3", "lastValue": 5.04, "min": 4.80, "max": 5.10, "interval": 3000 },
 
@@ -58,7 +58,7 @@ var devices = [ // описываем устройства
         "type": "WB-MSW v.3",
         "address": 5,
         "emulators": [
-            { "topicName": "Temperature", "type": "sensor2Dec", "name": "Bathroom Temperature", "lastValue": 28, "min": 15, "max": 60, "interval": 10000 }, // interval (ms) >=1000 ms
+            { "topicName": "Temperature", "type": "sensor2Dec", "name": "Bathroom Temperature", "lastValue": 28, "min": 20, "max": 30, "interval": 10000 }, // interval (ms) >=1000 ms
             { "topicName": "Humidity", "type": "sensor2Dec", "name": "Bathroom Humidity", "lastValue": 65, "min": 10, "max": 80, "interval": 10000 },
             { "topicName": "VOC", "type": "sensor0Dec", "name": "Bathroom VOC", "lastValue": 900, "min": 500, "max": 4000, "interval": 10000 },
             { "topicName": "CO2", "type": "sensor0Dec", "name": "Bathroom CO2", "lastValue": 1200, "min": 450, "max": 2000, "interval": 10000 },
