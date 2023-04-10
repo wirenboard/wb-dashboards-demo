c9f7acdaa9cbd944439d243a5b7543768859ac53
wb-adegtyarev <a.degtyarev@wirenboard.com>
2023-04-10 15:07:41 +0400

    Починил эмулятор — теперь наименования устройств и контролов такие же, как у реальных устройств

--- house/4in/scripts/4in-panel-logic.js
+++ house/4in/scripts/4in-panel-logic.js
@@ -1,5 +1,5 @@
 defineRule({
-    whenChanged: ["wb-msw-v3-5/Temperature"],
+    whenChanged: ["wb-msw-v3_5/Temperature"],
     then: function (newValue, devName, cellName) {
         dev["4in-panel-thermostat/CurrTemp"] = newValue
 
